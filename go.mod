c97ec8311e6e41763ae9ade2b29b8e764d1ccf51
bpriyatmojo <budhi.priyatmojo@amartha.com>
2022-02-21 01:01:30 +0700

    update logger

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/budhip/common
 go 1.14
 
 require (
+	github.com/blendle/zapdriver v1.3.1
 	github.com/go-sql-driver/mysql v1.5.0
-	go.uber.org/zap v1.21.0 // indirect
+	go.uber.org/zap v1.21.0
 )
--- logger/logger.go
+++ logger/logger.go
@@ -1,17 +1,17 @@
 package logger
 
 import (
-	"context"
-
+	"github.com/blendle/zapdriver"
 	"go.uber.org/zap"
 )
 
-// Logger Interface. All methods SHOULD be safe for concurrent use.
-type Logger interface {
-	// Info logs a message at Info level
-	Info(ctx context.Context, msg string, fields ...zap.Field)
-	// Warn logs a message at Warn level
-	Warn(ctx context.Context, msg string, fields ...zap.Field)
-	// Error logs a message at Errors level
-	Error(ctx context.Context, msg string, err error)
+func NewLogger() *zap.SugaredLogger {
+	logger, err := zapdriver.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	sugar := logger.Sugar()
+
+	return sugar
 }
