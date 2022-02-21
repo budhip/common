805afe17f70eea5aab6576544e0bd139455df28e
bpriyatmojo <budhi.priyatmojo@amartha.com>
2022-02-22 02:52:08 +0700

    update postgre

--- postgre/postgre.go
+++ postgre/postgre.go
@@ -1,20 +1,9 @@
 package postgre
 
 import (
-	"fmt"
-	"time"
-
 	"database/sql"
+	"fmt"
 	"net/url"
-)
-
-const (
-	// DefaultMaxOpen is default value for max open connection
-	DefaultMaxOpen = 10
-	// DefaultMaxIdle is default value for max idle connection
-	DefaultMaxIdle = 10
-	// DefaultMaxLifetime is default value for max connection lifetime in minutes
-	DefaultMaxLifetime = 3
 )
 
 type Config struct {
@@ -23,29 +12,12 @@ type Config struct {
 	User        string
 	Password    string
 	Name        string
-	MaxOpen     int
-	MaxIdle     int
-	MaxLifetime int // in minutes
-	MaxIdleTime int // in minutes
-	ServerName  string
-	ParseTime   bool
-	Location    string
 }
 
 func dataSourceName(config Config) string {
 	connection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, config.Password, config.Name)
 	val := url.Values{}
 
-	if config.ParseTime {
-		val.Add("parseTime", "1")
-	}
-	if len(config.Location) > 0 {
-		val.Add("loc", config.Location)
-	}
-
-	if len(val) == 0 {
-		return connection
-	}
 	return fmt.Sprintf("%s?%s", connection, val.Encode())
 }
 
@@ -55,28 +27,6 @@ func DB(config Config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	if config.MaxOpen > 0 {
-		db.SetMaxOpenConns(config.MaxOpen)
-	} else {
-		db.SetMaxOpenConns(DefaultMaxOpen)
-	}
-
-	if config.MaxIdle > 0 {
-		db.SetMaxIdleConns(config.MaxIdle)
-	} else {
-		db.SetMaxIdleConns(DefaultMaxIdle)
-	}
-
-	if config.MaxLifetime > 0 {
-		db.SetConnMaxLifetime(time.Duration(config.MaxLifetime) * time.Minute)
-	} else {
-		db.SetConnMaxLifetime(time.Duration(DefaultMaxLifetime) * time.Minute)
-	}
-
-	// if config.MaxIdleTime > 0 {
-	// 	db.SetConnMaxIdleTime(time.Duration(config.MaxIdleTime) * time.Minute)
-	// }
 
 	return db, nil
 }
